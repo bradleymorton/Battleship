ccdaa919a9117cf59a88df6d834db3654250a4ed
bradleymorton <bradley.m.morton@gmail.com>
2019-02-22 16:11:09 -0900

    working on changes.

--- battleship.py
+++ battleship.py
@@ -3,20 +3,19 @@
 
 class Game():
     def __init__(self):
-        baordSize = 10
-        playerShipBoard     = [['*']*boardSize, ['*']*boardSize]
-        playerTargetBoard   = [['*']*boardSize, ['*']*boardSize]
-        aiShipBoard         = [['*']*boardSize, ['*']*boardSize]
-        aiTargetBoard       = [['*']*boardSize, ['*']*boardSize]
+		boardSize = 10
+		board = [['*']*boardSize, ['*']*boardSize]
+		playerShipBoard     = board
+		playerTargetBoard   = board
+		aiShipBoard         = board
+		aiTargetBoard       = board
 
-        playerHitCount  = 0
-        aiHitCount      = 0
-
-        playGame = True
-        winner = ""
-
-        playerShips = [['c','c','c','c','c'],['b','b','b','b'],['r','r','r'],['s','s','s'],['d','d']] #fix later
-        aiShips     = [['c','c','c','c','c'],['b','b','b','b'],['r','r','r'],['s','s','s'],['d','d']]
+		playerHitCount  = 0
+		aiHitCount      = 0
+		playGame = True
+		winner = ""
+		playerShips = [['c','c','c','c','c'],['b','b','b','b'],['r','r','r'],['s','s','s'],['d','d']] #fix later
+		aiShips     = [['c','c','c','c','c'],['b','b','b','b'],['r','r','r'],['s','s','s'],['d','d']]
 
     def validSpot(self,x,y): # only checks if on board
         validSpot = False
@@ -39,10 +38,10 @@ class Game():
                 elif direction == 'w' and validSpot(x,y) and validSpot(x-1,y)and validSpot(x-2,y) and validSpot(x-3,y) and validSpot(x-4,y):
                     for i in range(0,5):
                      playerShipBoard[x-i][y] = letter
-            elif letter == 'b':
+			elif letter == 'b':
             elif letter == 'r':
-            elif letter == 's':
-            elif letter == 'd' :
+			elif letter == 's':
+			elif letter == 'd' :
         return
 
     def fire(self,team,x,y):
@@ -58,7 +57,7 @@ class Game():
             if validSpot(x,y) and playerShipBoard[x][y] == letter : # hit   FIX!!!!!!!!
                 aiHitCount              += 1
                 aiTargetBoard[x][y]     = 'X'
-                playerShipbaord[x][y]   = 'X'
+                playerShipboard[x][y]   = 'X'
             elif validSpot(x,y):                                    # miss
                 playerTargetBoard[x][y] = '@'
                 aiShipbaord[x][y]       = '@'
@@ -74,7 +73,20 @@ class Game():
         return
 
 
-def draw(self):
+def draw(self, team):
+	if team == player:
+		print("This is the target board")
+		for x in range(boardSize):
+			for y in range(boardSize):
+				print(playerTargetBoard[x][y])
+			print("\n")
+		print("This is your board")
+		for x in range(boardSize):
+			for y in range(boardSize):
+				print(playerShipBoard[x][y])
+			print("\n")
+
+
     return
 
 
