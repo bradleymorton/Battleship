ad1c7816e0c0a1706fc15f468add807f8eedf11b
georgetmeier <gtmeier@alaska.edu>
2019-02-25 17:56:12 -0900

    testing game play mechanics

--- battleship.py
+++ battleship.py
@@ -7,11 +7,10 @@
 class Game():
     def __init__(self):
         self.boardSize = 10
-        self.board = [["*" for i in range(self.boardSize)] for j in range(self.boardSize)]
-        self.playerShipBoard     = self.board
-        self.playerTargetBoard   = self.board
-        self.aiShipBoard         = self.board
-        self.aiTargetBoard       = self.board
+        self.playerShipBoard     = [["*" for i in range(self.boardSize)] for j in range(self.boardSize)]
+        self.playerTargetBoard   = [["*" for i in range(self.boardSize)] for j in range(self.boardSize)]
+        self.aiShipBoard         = [["*" for i in range(self.boardSize)] for j in range(self.boardSize)]
+        self.aiTargetBoard       = [["*" for i in range(self.boardSize)] for j in range(self.boardSize)]
 
         self.playerHitCount  = 0
         self.aiHitCount      = 0
@@ -20,13 +19,13 @@ class Game():
 
         # Ships = [['c','c','c','c','c'],['b','b','b','b'],['r','r','r'],['s','s','s'],['d','d']] #note
 
-    def validSpot(self,x,y): # only checks if on board
+    def validSpot(self, x, y): # only checks if on board
         valid = False
-        if x>=0 and x<=self.boardSize and y>=0 and y<=self.boardSize:
-            validSpot = True
+        if x>=0 and x<=self.boardSize and y<=self.boardSize:
+            valid = True
         return valid
 
-    def place(self,team,x,y,direction,letter):
+    def place(self, team, x, y, direction, letter):
         if team == "player":
             if letter == 'c' :
                 if direction == 'n' and self.validSpot(x,y) and self.validSpot(x,y+1)and self.validSpot(x,y+2) and self.validSpot(x,y+3) and self.validSpot(x,y+4):
@@ -82,19 +81,15 @@ class Game():
                         self.playerShipBoard[x-i][y] = letter
             elif letter == 'd':
                 if direction == 'n' and self.validSpot(x,y) and self.validSpot(x,y+1):
-                    print("placing destroyer, direction north")
                     for i in range(0,2):
                         self.playerShipBoard[x][y+i] = letter
                 elif direction == 's' and self.validSpot(x,y) and self.validSpot(x,y-1):
-                    print("placing destroyer, direction south")
                     for i in range(0,2):
                         self.playerShipBoard[x][y-i] = letter
                 elif direction == 'e' and self.validSpot(x,y) and self.validSpot(x+1,y):
-                    print("placing destroyer, direction north")
                     for i in range(0,2):
                         self.playerShipBoard[x+i][y] = letter
                 elif direction == 'w' and self.validSpot(x,y) and self.validSpot(x-1,y):
-                    print("placing destroyer, direction north")
                     for i in range(0,2):
                         self.playerShipBoard[x-i][y] = letter
         elif team == "ai":
@@ -170,10 +165,10 @@ class Game():
             if self.validSpot(x,y) and self.aiShipBoard[x][y] in ('c','b','r','s','d') : # hit
                 self.playerHitCount          += 1
                 self.playerTargetBoard[x][y] = 'X'
-                self.aiShipbaord[x][y]       = 'X'
+                self.aiShipBoard[x][y]       = 'X'
             elif self.validSpot(x,y):                                # miss
                 self.playerTargetBoard[x][y] = '@'
-                self.aiShipbaord[x][y]       = '@'
+                self.aiShipBoard[x][y]       = '@'
         elif team == "ai":
             if self.validSpot(x,y) and self.playerShipBoard[x][y] in ('c','b','r','s','d') : # hit
                 self.aiHitCount              += 1
@@ -181,7 +176,7 @@ class Game():
                 self.playerShipBoard[x][y]   = 'X'
             elif self.validSpot(x,y):                                    # miss
                 self.playerTargetBoard[x][y] = '@'
-                self.aiShipbaord[x][y]       = '@'
+                self.aiShipBoard[x][y]       = '@'
             return
 
     def checkWin(self):
@@ -196,37 +191,42 @@ class Game():
 
     def draw(self, team):
         if team == "player":
-            print("This is the target board")
-            for x in range(self.boardSize):
-                for y in range(self.boardSize):
+            print("This is the player target board")
+            for y in range(self.boardSize):
+                for x in range(self.boardSize):
                     print(self.playerTargetBoard[x][y], end = '')
                 print("")
-            print("This is your board")
-            for x in range(self.boardSize):
-                for y in range(self.boardSize):
+            print("This is the player board")
+            for y in range(self.boardSize):
+                for x in range(self.boardSize):
                     print(self.playerShipBoard[x][y], end = '')
                 print("")
         elif team == "ai":
-            print("This is the target board")
-            for x in range(self.boardSize):
-                for y in range(self.boardSize):
-                    print(self.aiTargetBoard[x][y])
-                print("\n")
-            print("This is your board")
-            for x in range(self.boardSize):
-                for y in range(self.boardSize):
-                    print(self.aiShipBoard[x][y])
-                print("\n")
+            print("This is the ai target board")
+            for y in range(self.boardSize):
+                for x in range(self.boardSize):
+                    print(self.aiTargetBoard[x][y], end = '')
+                print("")
+            print("This is the ai board")
+            for y in range(self.boardSize):
+                for x in range(self.boardSize):
+                    print(self.aiShipBoard[x][y], end = '')
+                print("")
         return
 
 
 
 game = Game()
-game.place("player", 5, 5, 'n', 'd')
-game.draw("player") 
-game.place("player", 5, 5, 'n', 'd')
-game.draw("player") 
-
+# game.draw("player")
+game.place("player", 0, 0, 'e', 'd')
+# game.draw("player")
+# game.draw("ai")
+# game.fire("ai",0,0)
+# game.draw("player")
+# game.draw("ai")
+game.fire("player",0,0)
+game.draw("player")
+game.draw("ai")
 
 
 
